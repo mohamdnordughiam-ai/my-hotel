96ce4ee3283d6b79172b40c91512c394f45366bc
mohamdnordughiam-ai <mohamdnor.dughiam@elev.ga.lbs.se>
2026-06-05 10:33:45 +0200

    register,

---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+
+$conn = new mysqli("localhost", "eliot" , "server" , "Nour");
+
+if ($_SERVER["REQUEST_METHOD"] == "post") {
+    // om det är en POST request
+    $username = $_POST ["username"];
+    $password = $_POST["password"];
+    $sql = "INSERT INTO formdata (name, password) VALUES ('$title', '$beskrivning')";
+
+    $conn->query($sql);
+
+
+}  
+
+?>
+
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+    username: <input type="text" name="username" required><br><br>
+    password:<br>
+    <textarea name="password" rows="5" cols="40"></textarea><br><br>
+    <input type="submit" value="Skicka">
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="test.php" method="post">
+        <label>username:</label>
+        <input type="text" name="username">
+        <label>password:</label>
+        <input type="password" name="password">
+         <input type="submit">
+    </form>
+    
+</body>
+</html>
+
+
+</form>
+
+
+
+    
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="test.php" method="post">
+        <label>username:</label>
+        <input type="text" name="username">
+        <label>password:</label>
+        <input type="password" name="password">
+         <input type="submit">
+
+
+
+    </form>
+    
+</body>
+</html>
